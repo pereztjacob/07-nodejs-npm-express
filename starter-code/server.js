904f78e4a3d315e0bbd50e1be212df2ad36824c8
pereztjacob <pereztjacob@gmail.com>
2017-12-01 13:25:11 -0800

    packages added

--- starter-code/server.js
+++ starter-code/server.js
@@ -4,7 +4,8 @@
 const express = require('express');
 const app = express();
 
-app.use( express.static('./public'));
+app.use(express.static('./public'));
+
 // TODO: Require the ExpressJS package that you installed via NPM, and instantiate the app.
 // Remember to install ExpressJS, and be sure that it's been added to your package.json as a dependency.
 // There is also a package here called body-parser, which is used by the provided POST route. Be sure to install that and save it as a dependency after you create your package.json.
@@ -25,10 +26,17 @@ app.post('/articles', bodyParser, function(request, response) {
 });
 
 // TODO: Write a new route, using an arrow function, that will handle a request and send the new.html file back to the user
+app.get('/', (request, response) => {
+    response.sendFile('./public/new.html', {root: '.'});
+});
 
 // TODO: Write a new route, using an arrow function, that will handle any other routes that were not defined and deliver a 404 status message to the user
+app.get('/', (request, response) => {
+    response.sendFile('404', {root: '.'});
+});
 
-app.listen(PORT, function() {
+
+app.listen(PORT, () => {
     // TODO: Refactor this to arrow function, log to the console a message that lets you know which port your server has started on
-
+    console.log(`Listening on port ${PORT}`);
 });
